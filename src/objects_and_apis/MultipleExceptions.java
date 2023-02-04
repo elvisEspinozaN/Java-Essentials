b03a1a6481916952eb5c1d1cd863b3d896259e06
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-02-04 14:01:12 -0500

    Difference between try-resources and finally

--- src/objects_and_apis/MultipleExceptions.java
+++ src/objects_and_apis/MultipleExceptions.java
@@ -2,6 +2,7 @@ package objects_and_apis;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,18 +10,29 @@ public class MultipleExceptions {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File file = new File("files/numbers.txt");
-        try {
-            Scanner fileReader = new Scanner(file);
+        File inputFile = new File("files/numbers.txt");
+        File outputFile = new File(("files/output.txt"));
 
+        /*
+        try-with-resources : declaration of resources that should be automatically
+        closed after the execution of a try block
+        can use several parameters
+        resources are closed in the reverse order they were defined
+         */
+        try(
+                Scanner fileReader = new Scanner(inputFile);
+                PrintWriter fileWriter = new PrintWriter(outputFile)
+        ) { // initialize the resource
             while(fileReader.hasNext()) {
-                System.out.println(fileReader.hasNextDouble()); // nextDouble method has unchecked exception
+                fileWriter.println(fileReader.nextDouble());
             }
         } catch(FileNotFoundException | InputMismatchException e) {
             e.printStackTrace();
-        } catch(Exception e) {
-            e.printStackTrace();
         }
+        // this won't be needed
+//        finally { // execute no matter what happen with the try or catch
+//            fileReader.close();
+//        }
 
     }
 }
