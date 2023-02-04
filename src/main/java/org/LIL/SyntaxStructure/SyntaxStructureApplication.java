4017477471d64d412f409903b9f1efc320c79dd0
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-02-04 18:14:16 -0500

    Spring Boot starting up...

---
+++ src/main/java/org/LIL/SyntaxStructure/SyntaxStructureApplication.java
@@ -0,0 +1,13 @@
+package org.LIL.SyntaxStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxStructureApplication.class, args);
+	}
+
+}
